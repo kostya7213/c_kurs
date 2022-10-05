6d996126dd7596e96829547f376dd281d89793bf
kostya <kostya.bochkarev2015@yandex.ru>
2022-10-05 22:55:41 +0300

    вторая задача

---
+++ первое занятие с#/2 задача/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисе
+Console.WriteLine("Введите числа");
+int number_1 = int.Parse(Console.ReadLine());
+int number_2 = int.Parse(Console.ReadLine());
+int number_3 = int.Parse(Console.ReadLine());
+int max = number_1;
+if (number_2 > max)
+    max = number_2;
+if (number_3 > max)
+    max = number_3;
+Console.WriteLine($"max = {max}");
