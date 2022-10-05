8203c218a1aa106147240d2ddc20481f69ad9d38
kostya <kostya.bochkarev2015@yandex.ru>
2022-10-05 22:33:28 +0300

    первая задача(первое занятие)

---
+++ первое занятие с#/1 задача/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите числа: ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a < b)
+{
+    Console.WriteLine($"min = {a}");
+    Console.WriteLine($"max = {b}");
+} else if (a > b)
+{
+    Console.WriteLine($"min = {b}");
+    Console.WriteLine($"max = {a}");
+} else 
+    Console.WriteLine("Числа равны");
