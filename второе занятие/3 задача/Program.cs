6c2c66e3e7ae90b68000f3d247f7735b7c072dab
kostya <kostya.bochkarev2015@yandex.ru>
2022-10-06 13:39:45 +0300

    (02) третье задание(v2.1)

--- второе занятие/3 задача/Program.cs
+++ второе занятие/3 задача/Program.cs
@@ -1,24 +1,35 @@
 ﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 void opr_day (int num)
 {
-    if (num == 1) 
-    Console.WriteLine("Понедельник");
-else if (num == 2) 
-    Console.WriteLine("вторник");
-else if (num == 3) 
-    Console.WriteLine("среда");
-else if (num == 4) 
-    Console.WriteLine("четверг");
-else if (num == 5) 
-    Console.WriteLine("пятница");
-else if (num == 6) 
-    Console.WriteLine("суббота");
-else if (num == 7) 
-    Console.WriteLine("воскресенье");
-else 
-    Console.WriteLine("так сложилось, но в неделе всего 7 дней");
+  switch (num)
+  {
+    case 1:
+        Console.WriteLine("Понедельник");
+    break;
+    case 2:
+        Console.WriteLine("Вторник");
+    break;
+    case 3:
+        Console.WriteLine("Среда");
+    break;
+    case 4:
+        Console.WriteLine("Четверг");
+    break;
+    case 5:
+        Console.WriteLine("Пятница");
+    break;
+    case 6:
+        Console.WriteLine("Суббота");
+    break;
+    case 7:
+        Console.WriteLine("Воскресенье");
+    break;
+    default: 
+         Console.WriteLine("так сложилось, но в неделе всего 7 дней");
+    break;
+  }
+    
 }
-
 Console.Write("введите число ");
 int number = int.Parse(Console.ReadLine());
 opr_day (number);
