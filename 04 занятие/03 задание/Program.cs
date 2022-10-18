b62a3ad56a09a785de7d830988c80e2616106d48
kostya <kostya.bochkarev2015@yandex.ru>
2022-10-18 11:16:52 +0300

    04 занятие 03 задание

---
+++ 04 занятие/03 задание/Program.cs
@@ -0,0 +1,43 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Элементы задаются рандомно 
+//(диапазон выбрать самостоятельно) Итоговый массив должен содержать в себе суммы цифр числа. 
+//Т.е. необходимо подсчитывать суммы цифр каждого числа и записывать их в новый массив.
+void show (int[] a, int N)
+{
+    for (int i = 0; i < N; i++)
+        Console.Write($"{a[i]} ");
+        Console.WriteLine();
+}
+void fill (int[] a, int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        a[i] = new Random().Next(1, 100);
+        
+    }
+}
+void sum_element_array(int[] a, int N)
+{
+    for (int i = 0 ; i < N; i++)
+    {
+        int num = a[i];
+        int sum = 0;
+        while (num != 0)
+        {
+            sum = sum + (num % 10);
+            num /= 10;
+        }
+        a[i] = sum;
+    }
+}
+int _N = 8;
+int [] array = new int [_N];
+
+fill( array, _N );
+Console.Write("Изначальный массив: ");
+show(array, _N );
+sum_element_array(array, _N);
+Console.Write("Измененный массив: ");
+show(array, _N );
+
+
+
