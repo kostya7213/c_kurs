4dc477371850f4f6a469d05cd94a45a222ae7c55
kostya <kostya.bochkarev2015@yandex.ru>
2022-10-18 10:22:00 +0300

    04 занятие 01 задание

--- 04 занятие/01 задание/Program.cs
+++ 04 занятие/01 задание/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Сделать в функции, сделать проверку на отрицательность.
+
+double sqr (double a, double b)
+{
+    double c = a;
+    int i = 1;
+    if (b == 0)
+    {
+        return 1;
+    }
+    else if (b > 0)
+    {
+        while ( i < b)
+        {
+            c *= a;
+            i++;
+        }
+        return c;
+    }
+    else if (b < 0)
+    {
+        i = -1;
+        while ( i > b)
+        {
+            c *= a;
+            i--;
+        }
+        return 1/c;
+    }
+
+return c;
+
+}
+
+Console.Write("Введите число А: ");
+Double _a = double.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+Double _b = double.Parse(Console.ReadLine());
+double _c; 
+_c = sqr(_a, _b);
+Console.Write($"{_a} в степени {_b} равно {_c}");
+
+
