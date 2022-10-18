340b30cb1278ee699a6c55283d1ef512dd5c57f5
kostya <kostya.bochkarev2015@yandex.ru>
2022-10-18 11:38:45 +0300

    05 занятие 01 задание

---
+++ 05 занятие/01 задание/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void fill (int[] a, int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        a[i] = new Random().Next(100,999);
+        
+    }
+}
+void show (int[] a, int N)
+{
+    for (int i = 0; i < N; i++)
+        Console.Write($"{a[i]} ");
+        Console.WriteLine();
+}
+int chetnost (int[] a, int N)
+{
+    int sum = 0; 
+    for (int i = 0; i < N; i++)
+    {
+        if (a[i] % 2 == 0)
+            sum++;
+    }
+    return sum;
+}   
+
+int _N = 10;
+int [] array = new int [_N];
+fill (array, _N);
+show(array, _N);
+int s = 0; 
+s = chetnost(array, _N);
+Console.WriteLine($"Количество четных элементов: {s}");
