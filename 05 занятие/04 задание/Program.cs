4552b61099ff676737f4091c9909c212da6001c7
kostya <kostya.bochkarev2015@yandex.ru>
2022-10-21 11:41:06 +0300

    05 занятие 04 задание

---
+++ 05 занятие/04 задание/Program.cs
@@ -0,0 +1,30 @@
+﻿// Дан список некоторых целых чисел, найдите значение 20 в нем и, если оно присутствует, замените его на 200. Обновите список только при первом вхождении числа 20.
+
+void show (List<int> a)
+{
+    for (int i = 0; i < a.Count; i++)
+        Console.Write($"{a[i]} ");
+        Console.WriteLine();
+}
+
+void exchange (List <int> a)
+{
+     for (int i = 0; i < a.Count; i++)
+     {
+        if (a[i] == 20)
+        {
+            a[i] = 200;
+            break;
+        }
+     }
+}
+
+
+int _N = 5;
+List<int> array = new List<int>(_N) {21, 53,20, 56, 45,20};
+//fill(array);
+Console.Write("Изначальный список: ");
+show(array);
+exchange(array);
+Console.Write("Изменненый список: ");
+show(array);
