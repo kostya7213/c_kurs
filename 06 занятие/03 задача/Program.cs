5b4769f49765f79108b28963d54ce6a5e0812e17
kostya <kostya.bochkarev2015@yandex.ru>
2022-10-24 23:11:24 +0300

    06 занятие 03 задача

---
+++ 06 занятие/03 задача/Program.cs
@@ -0,0 +1,36 @@
+﻿/*	
+0 1 2 3 4
+1 0 1 2 3
+2 1 0 1 2
+3 2 1 0 1
+4 3 2 1 0*/
+int[,] array_diag(int [,] a, int N)
+{
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+        {
+            if (j <= i)
+                a[i, j] = i - j;
+            else 
+                a[i, j] = j - i;
+        }
+    return a;
+}
+void show (int[,] a, int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+            Console.Write($"{a[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int _N;
+Console.WriteLine("Введите число N");
+_N = int.Parse(Console.ReadLine());
+int [,] array = new int [_N ,_N]; 
+
+array = array_diag(array, _N);
+show(array, _N);
+
